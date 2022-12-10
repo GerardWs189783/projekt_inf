fc85a1fefb5646ebd5d2f0c4354fa8f2599b0e6b
GerardWs189783 <s189783@student.pg.edu.pl>
2022-12-10 20:20:17 +0100

    main

--- kody/Project1/main.cpp
+++ kody/Project1/main.cpp
@@ -3,181 +3,10 @@
 #include <random>
 #include "Game.h"
 #include "paddle.h"
+#include "ball.h"
+#include "block.h"
 
-std::random_device rd;
 
-class Ball
-{
-private:
-	sf::Vector2f position;
-	float xVel = 2.5, yVel = 2.5;
-	sf::Texture tekstura;
-	sf::Sprite pSprite;
-	sf::Vector2f romzmiar_okna;
-	sf::FloatRect ballbound;
-public:
-	Ball(float x_in, float y_in, float rx_in, float ry_in)
-	{
-		position.x = x_in;
-		position.y = y_in;
-		//romzmiar_okna.x = rx_in - 70;
-		//romzmiar_okna.y = ry_in - 70;
-		tekstura.loadFromFile("zdj/shrokjj.png");
-		pSprite.setTexture(tekstura);
-		pSprite.setPosition(position);
-		pSprite.setScale(sf::Vector2f(70.f / pSprite.getLocalBounds().width, 70.f / pSprite.getLocalBounds().height));
-		/*pSprite.setOrigin(pSprite.getGlobalBounds().width/2.f, pSprite.getGlobalBounds().height/2.f);*/
-		romzmiar_okna.x = rx_in - pSprite.getGlobalBounds().width /*-70*/;
-		romzmiar_okna.y = ry_in - pSprite.getGlobalBounds().height /*- 70*/;
-		ballbound = pSprite.getGlobalBounds();
-	}
-	void przesun(float x_in, float y_in)
-	{
-		sf::Vector2f pos;
-		pos.x = x_in;
-		pos.y = y_in;
-		pSprite.move(pos);
-		position = pSprite.getPosition();
-		
-	}
-	sf::Sprite getBall() { return pSprite; }
-	void sprawdzKolizjeSciany()
-	{
-		if (position.x <= 0)
-			xVel = abs(xVel);
-		if (position.x >= romzmiar_okna.x)
-			xVel = -abs(xVel);
-		if (position.y <= 0)
-			yVel = abs(yVel);
-		if (position.y >= romzmiar_okna.y)
-			yVel = -abs(yVel);
-	}
-	void animuj(sf::Sprite sprite)
-	{
-		sprawdzKolizjeSciany();
-		sprawdzKolizjeObiektu(sprite);
-		przesun(xVel, yVel);
-	}
-	void sprawdzKolizjeObiektu(sf::Sprite sprite)
-	{
-		sf::FloatRect overlap = sprite.getGlobalBounds();
-		if (pSprite.getGlobalBounds().intersects(sprite.getGlobalBounds())) {
-			
-			//right
-			//if (ballbound.left < overlap.left && ballbound.left + ballbound.width < overlap.left + overlap.width
-			//	&& ballbound.top < overlap.top + overlap.height
-			//	&& ballbound.top + ballbound.height < overlap.top)
-			//{
-			//	pSprite.setPosition(overlap.left - ballbound.width, ballbound.top);
-			//	yVel *= -1;
-			//}
-			////left
-			//else if (ballbound.left > overlap.left && ballbound.left + ballbound.width > overlap.left + overlap.width
-			//	&& ballbound.top < overlap.top + overlap.height
-			//	&& ballbound.top + ballbound.height < overlap.top)
-			//{
-			//	pSprite.setPosition(overlap.left + ballbound.width, ballbound.top);
-			//	yVel *= -1;
-			//}
-			////bottom
-			//if (ballbound.top < overlap.top && ballbound.top + ballbound.height < overlap.top + overlap.height
-			//	&& ballbound.left < overlap.left + overlap.width
-			//	&& ballbound.left + ballbound.width < overlap.left)
-			//{
-			//	pSprite.setPosition(ballbound.left, overlap.top - ballbound.height);
-			//	yVel *= -1;
-			//}
-			////top
-			//else if (ballbound.top > overlap.top && ballbound.top + ballbound.height > overlap.top + overlap.height
-			//	&& ballbound.left < overlap.left + overlap.width
-			//	&& ballbound.left + ballbound.width < overlap.left)
-			//{
-			//	pSprite.setPosition(ballbound.left, overlap.top - overlap.height);
-			//	yVel *= -1;
-			//}
-			//Lewa czêœæ pi³ki
-			/*if (sprite.getPosition().x + sprite.getGlobalBounds().width - pSprite.getPosition().x - pSprite.getGlobalBounds().width < 0)
-			{
-				xVel *= -1;
-				pSprite.move(sf::Vector2f((-1) * (sprite.getPosition().x + sprite.getGlobalBounds().width - pSprite.getPosition().x - pSprite.getGlobalBounds().width), 0.f));
-			}*/
-			//Najlepiej dzia³a na lew¹ stronê resztynie dotyka ale jest to chyba robocze
-			if (pSprite.getPosition().x < sprite.getPosition().x + (0.5)*sprite.getGlobalBounds().width &&
-				pSprite.getPosition().x+pSprite.getGlobalBounds().width > sprite.getPosition().x - (0.5) * sprite.getGlobalBounds().width &&
-				abs(sprite.getPosition().x + (0.5) * sprite.getGlobalBounds().width - pSprite.getPosition().x) < abs(sprite.getPosition().y + (0.5) * sprite.getGlobalBounds().height - pSprite.getPosition().y) /*&&
-				pSprite.getPosition().y+pSprite.getGlobalBounds().height > sprite.getPosition().y - (0.5)*sprite.getGlobalBounds().height*/
-				&& sprite.getPosition().x + (0.5) * sprite.getGlobalBounds().width - pSprite.getPosition().x <(pSprite.getGlobalBounds().width)/2
-				&& pSprite.getPosition().x + pSprite.getGlobalBounds().width > sprite.getPosition().x - (0.5) * sprite.getGlobalBounds().width)
-			{
-				xVel *= -1;
-				pSprite.move(sf::Vector2f(sprite.getPosition().x+ (0.5) * sprite.getGlobalBounds().width - pSprite.getPosition().x, 0.f));
-			}
-			//prawa strona pi³ki
-			else if (pSprite.getPosition().x + pSprite.getGlobalBounds().width > sprite.getPosition().x - (sprite.getGlobalBounds().width)/2.f &&
-				pSprite.getPosition().x < sprite.getPosition().x - (0.5)*sprite.getGlobalBounds().width &&
-				pSprite.getPosition().x < sprite.getPosition().x + (0.5) * sprite.getGlobalBounds().width)
-			{
-				xVel *= -1;
-				pSprite.move(sf::Vector2f(pSprite.getPosition().x + pSprite.getGlobalBounds().width - sprite.getPosition().x +(sprite.getGlobalBounds().width) / 4.f , 0.f));
-			}
-			//Góra i dó³
-			/*else if ()*/
-			yVel *= -1;
-		}
-		/*if(position.x<sprite.getPosition().x + sprite.getGlobalBounds().width && position.x+pSprite.getGlobalBounds().width > sprite.getPosition().x
-			&& position.y<sprite.getPosition().y + sprite.getGlobalBounds().height && position.y + pSprite.getGlobalBounds().height > sprite.getPosition().y)
-			yVel *= -1;*/
-	}
-};
-
-class Block {
-private:
-	sf::Vector2f pos;
-	sf::Texture tekstura;
-	sf::Sprite block;
-
-public:
-	Block(float x_in,float y_in);
-	void bdraw(sf::RenderWindow& window);
-	void move();
-	sf::Sprite getSprite();
-};
-
-Block::Block(float x_in, float y_in)
-{
-	pos.x = x_in;
-	pos.y = y_in;
-	tekstura.loadFromFile("zdj/blockani.png");
-	block.setTexture(tekstura);
-	block.setTextureRect(sf::IntRect(3, 3, 48, 18));
-	block.setPosition(pos.x, pos.y);
-	block.setScale(sf::Vector2f(75.f / block.getGlobalBounds().width, 30.f / block.getGlobalBounds().height));
-	block.setOrigin(sf::Vector2f(block.getLocalBounds().width / 2.f, block.getLocalBounds().height / 2.f));
-}
-void Block::bdraw(sf::RenderWindow& window)
-{
-	window.draw(block);
-}
-
-void Block::move()
-{
-	float x = 0, y = 0;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> distX(0,700);
-	std::uniform_int_distribution<> distY(0,200);
-	x = distX(gen);
-	y = distY(gen);
-	/*if(!((x< block.getGlobalBounds().width / 2.f) || (x > 800 - block.getGlobalBounds().width / 2.f) || (y<block.getGlobalBounds().height/2.f) ||(y>250)))*/
-	block.setPosition(x, y);
-	/*else*/
-	/*{
-		block.setPosition(distX(gen),200);
-	}*/
-}
-//Zrobiæ naliczanie punktów, sprite który zmienia skina przy uderzeniu?
-sf::Sprite Block::getSprite() {
-	return block;
-}
 
 int main()
 {
@@ -213,24 +42,25 @@ int main()
 
 		}
 
-
+		/*pb.scoreinit();*/
 		window.clear(sf::Color::Green);
 		p1.pdraw(window);
-		window.draw(pb.getBall());
+		/*window.draw(pb.getBall());*/
+		pb.drawt(window);
 		b1.bdraw(window);
 		window.display();
 		if (wait == true)
 		{
 			if (zegar.getElapsedTime().asMilliseconds() > 5.0f) {
-				pb.animuj(p1.getSprite());
+				pb.animuj(p1.getSprite(),b1.getSprite());
 				pb.sprawdzKolizjeObiektu(b1.getSprite());
 				zegar.restart();
 			}
-			if (z.getElapsedTime().asMilliseconds() > 1000.f)
+			/*if (z.getElapsedTime().asMilliseconds() > 1000.f)
 			{
 				b1.move();
 				z.restart();
-			}
+			}*/
 		}
 	}
 	return 0;
--- kody/Project1/test.cpp
+++ kody/Project1/test.cpp
@@ -1,16 +1,117 @@
-//#include <SFML/Graphics.hpp>
-//#include <iostream>
-//#include "Game.h"
-//#include "paddle.h"
+////#include <SFML/Graphics.hpp>
+////#include <iostream>
+////#include "Game.h"
+////#include "paddle.h"
+////
+////int main()
+////{
+////	Game game;
+////	while (game.running())
+////	{
+////	//Event poll w game
+////	//Tu tylko update i render
+////	game.update();
+////	game.render();
+////}
+////}
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <SFML/Graphics.hpp> 
+//#include <ctime> 
+//#include <iostream> 
+//#include <random> 
+//#include <string> 
+//#include <cstdio> 
+//#include <cstdlib>
+//class interfejs
+//{
+//public:
+//	interfejs(sf::Vector2f position);
+//	~interfejs();
+//	void draw(sf::RenderWindow& win);
+//	void setText(sf::Text* text1, sf::Text* text2, sf::Text* text3);
+//protected:
+//	sf::RectangleShape* int_1;
+//	sf::Font* font;
+//	sf::Text* text1;
+//	sf::Text* text2;
+//	sf::Text* text3;
+//	sf::Vector2f position;
+//	void init(sf::Vector2f widht_height);
+//};
+//interfejs::interfejs(sf::Vector2f position)
+//{
+//	this->position.x = position.x;
+//	this->position.y = position.y;
+//	sf::Vector2f width_height(756.f, 500);//windowsize.x /windowsize.y
+//	init(width_height);
+//}
+//interfejs::~interfejs()
+//{
+//	delete int_1;
+//	int_1 = NULL;
+//}
+//void interfejs::init(sf::Vector2f width_height)
+//{
+//	int_1 = new sf::RectangleShape;
+//	font = new sf::Font;
+//	text1 = new sf::Text;
+//	text2 = new sf::Text;
+//	text3 = new sf::Text;
+//	font->loadFromFile("zdj/arial.ttf");
+//	text1->setFont(*font);///??? 
+//	text2->setFont(*font);///??? 
+//	text3->setFont(*font);///??? 
+//
+//	int_1->setPosition(22.f, 55.f);
+//	int_1->setSize(width_height);
+//	int_1->setFillColor(sf::Color(0, 0, 255));
+//	setText(text1, text2, text3);
+//
+//}
+//void interfejs::draw(sf::RenderWindow& win)
+//{
+//	win.draw(*int_1);
+//	win.draw(*text1);
+//	win.draw(*text2);
+//	win.draw(*text3);
+//
+//}
+//void interfejs::setText(sf::Text* text1, sf::Text* text2, sf::Text* text3)
+//{
+//	text1->setString("Left top");
+//	text2->setString("Right top");
+//	text3->setString("Center bottom");
+//	text1->setCharacterSize(24);
+//	text2->setCharacterSize(24);
+//	text3->setCharacterSize(24);
+//	text1->setFillColor(sf::Color(135, 206, 255));
+//	text2->setFillColor(sf::Color(240, 248, 255));
+//	text3->setFillColor(sf::Color(234, 221, 202));
+//	text1->setPosition(100.f, 100.f);
+//	text2->setPosition(300.f, 300.f);
+//	text3->setPosition(500.f, 500.f);
+//}
+//
 //
 //int main()
 //{
-//	Game game;
-//	while (game.running())
+//	interfejs one(sf::Vector2f(800.f, 600.f));
+//	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML demo");
+//	while (window.isOpen())
 //	{
-//	//Event poll w game
-//	//Tu tylko update i render
-//	game.update();
-//	game.render();
-//}
+//		sf::Event event;
+//		while (window.pollEvent(event))
+//		{
+//			if (event.type == sf::Event::Closed)
+//				window.close();
+//		}
+//		window.clear();
+//		one.draw(window);
+//
+//		window.display();
+//	}
+//
+//
+//	return 0;
 //}
